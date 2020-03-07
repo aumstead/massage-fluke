e29c7776c0d64ee5147c2210a9437a5d0105b0fa
Andrew Umstead <andrewumstead@MacBook-Air-2.local>
2020-03-08 01:16:01 +0800

    fixed favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,10 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <meta name="description" content="Massage Fluke - Bowen and Thai Massage" />
+          <meta
+            name="description"
+            content="Massage Fluke - Bowen and Thai Massage"
+          />
           <link
             rel="stylesheet"
             href="//cdn.quilljs.com/1.2.6/quill.snow.css"
@@ -29,8 +32,24 @@ class MyDocument extends Document {
             rel="stylesheet"
           ></link>
 
-          <title>Massage Fluke</title>
-          <link rel="icon" href="/favicon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
